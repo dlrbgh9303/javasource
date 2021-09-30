c7b2862bbcc61bd98c8e7b03893f9b421562bc4b
kyuho <dlrbgh9303@gmail.com>
2021-09-30 14:39:27 +0900

    dept 수정

--- jdbc/src/dept/DeptDTO.java
+++ jdbc/src/dept/DeptDTO.java
@@ -1,15 +1,21 @@
 package dept;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
-
-	
-
-
-
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 
 
 
 public class DeptDTO {
-
+	private int deptNo;
+	private String dname;
+	private String loc;
 }
