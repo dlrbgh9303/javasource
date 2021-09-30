b7fbb073f995e09fa91a711cb47b43e7caea00c5
kyuho <dlrbgh9303@gmail.com>
2021-09-30 15:58:28 +0900

    database 커넥션

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,5 +1,38 @@
 package dept;
 
-public class DeptDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DeptDAO {
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
+		}
+    
+	
+	public static Connection geConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url,user,password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
