419f1d4ae9d804970f5cbf042c72baa937148898
kyuho <dlrbgh9303@gmail.com>
2021-10-01 11:35:08 +0900

    부서조회

--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -1,10 +1,18 @@
 package dept;
 
+import java.util.Scanner;
+
 public class DeptMain {
 
 	public static void main(String[] args) {
 		
-
+		boolean run = true;
+		Scanner sc = new Scanner(System.in);
+		while (run) {
+			System.out.println("***************");
+			System.out.println("1. 전체 부서 조회");
+			System.out.println("2. 특정 부서 조회");
+		}
 	}
 
 }
