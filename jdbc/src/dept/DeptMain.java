b6c3726d5e9b17e6120ad335d3e69098d237287e
kyuho <dlrbgh9303@gmail.com>
2021-10-01 11:53:44 +0900

    <<<<master 삭제

--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -12,14 +12,11 @@ public class DeptMain {
 			System.out.println("***************");
 			System.out.println("1. ÀüÃ¼ ºÎ¼­ Á¶È¸");
 			System.out.println("2. Æ¯Á¤ ºÎ¼­ Á¶È¸");
-<<<<<<< master
 			System.out.println("3. ºÎ¼­ Ãß°¡");
 			System.out.println("4. ºÎ¼­ ¼öÁ¤");
 			System.out.println("5. ºÎ¼­ »èÁ¦");
 			System.out.println("6.Á¾·á");
 			System.out.println("***************");
-=======
->>>>>>> cc90397 3,4,5 ë¶€ì„œì¶”ê°€
 		}
 	}
 
